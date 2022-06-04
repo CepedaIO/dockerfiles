b807fead05a1d5d235f2c276aa7d65f2947d6aba
Alfred Cepeda <alfred@cepeda.io>
2022-06-04 00:12:46 -0400

    Adds docker to dev image

--- dev-alpine/setup.sh
+++ dev-alpine/setup.sh
@@ -1,4 +1,3 @@
-apk update
-apk add curl git vim zsh
+apk add --update curl git vim zsh docker
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"bira\"/" ~/.zshrc
