c175aa1baeeb2f82a57df48e9d966bdd5c4a77d2
Alfred Cepeda <alfred@cepeda.io>
2022-06-04 00:12:46 -0400

    updates to dev image

--- dev/setup.sh
+++ dev/setup.sh
@@ -1,3 +1,3 @@
-apk add --update curl git vim zsh docker docker-compose
+apt-get install --update curl git vim zsh docker
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 sed -i "s/ZSH_THEME=.*/ZSH_THEME=\"bira\"/" ~/.zshrc
